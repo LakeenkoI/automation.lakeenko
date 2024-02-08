016ad8d2c514724674401a4ba50d852362c6e89e
LakeenkoI <158428881+LakeenkoI@users.noreply.github.com>
2024-02-08 09:47:38 +0300

    Update Main.java

--- src/main/java/automation/lakeenko/Main.java
+++ src/main/java/automation/lakeenko/Main.java
@@ -3,6 +3,6 @@ package automation.lakeenko;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello Alex!");
+        System.out.println("Hello Alex!!!!!");
     }
 }
